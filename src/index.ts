7ffefae233a298705a85851cb899168e2b517cf3
Jan Schill <janschill@me.com>
2021-02-14 00:06:15 +0100

    WIP basic express

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,64 @@
-import { config } from "./configuration"
-import { toKebabCase } from "./util/converter"
-
 import express from "express";
+import cors from "cors";
+import path from "path"
+
 import { SolidClient } from "./solid-client";
 import { SpotifyClient } from "./spotify-client";
 
-const app = express();
-const privateResourceUrl = `https://pod.inrupt.com/jan/${toKebabCase(config.appName)}/tracks.ttl`
-const publicResourceUrl = `https://pod.inrupt.com/jan/public/`
+import { config } from "./configuration"
+import { toKebabCase } from "./util/converter"
+import BaseRouter from "./routers/base";
 
+// const privateResourceUrl = `https://pod.inrupt.com/jan/${toKebabCase(config.appName)}/tracks.ttl`
+// const publicResourceUrl = `https://pod.inrupt.com/jan/public/`
 
-async function solid() {
-  const solidClient = new SolidClient({
-    clientId: config.solidClientId,
-    clientSecret: config.solidClientSecret,
-    refreshToken: config.solidRefreshToken,
-    oidcIssuer: config.solidOidcIssuer
-  });
+// async function solid() {
+//   const solidClient = new SolidClient({
+//     clientId: config.solidClientId,
+//     clientSecret: config.solidClientSecret,
+//     refreshToken: config.solidRefreshToken,
+//     oidcIssuer: config.solidOidcIssuer
+//   });
 
-  await solidClient.login();
+//   await solidClient.login();
 
-  if (solidClient.session) {
-    const response = await solidClient.fetch(privateResourceUrl);
-    console.log(response)
-  }
-}
+//   if (solidClient.session) {
+//     const response = await solidClient.fetch(privateResourceUrl);
+//     console.log(response)
+//   }
+// }
 
-async function spotify() {
-  const spotifyClient = new SpotifyClient({
-    clientId: config.spotifyClientId,
-    clientSecret: config.spotifyClientSecret
-  });
+// async function spotify() {
+//   const spotifyClient = new SpotifyClient({
+//     clientId: config.spotifyClientId,
+//     clientSecret: config.spotifyClientSecret,
+//     redirectUri: config.spotifyRedirectUri
+//   });
 
-  await spotifyClient.login();
-  const response = await spotifyClient.fetch();
-  console.log(response)
-}
+//   await spotifyClient.login();
+//   const response = await spotifyClient.fetch();
+//   console.log(response)
+// }
 
 // solid()
-spotify()
+// spotify()
+
+
+class Server {
+  public app = express();
+  public router = BaseRouter;
+}
+
+const server = new Server();
+
+server.app.use("/", server.router);
+server.app.use(cors());
+server.app.set('views', path.join(__dirname, '/views/'));
+
+server.app.set("view engine", "pug")
+
+const port = config.port || 5000;
+
+server.app.listen(port, () => {
+  console.log(`Listening on port ${port}: http://localhost:${port}`);
+});
