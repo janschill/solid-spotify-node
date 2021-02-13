adb6fdc81f54ffcd8b52ea7f731150c0fe11134d
Jan Schill <janschill@me.com>
2021-02-13 02:59:00 +0100

    Introduce Client

---
+++ src/spotify-client.ts
@@ -0,0 +1,62 @@
+import SpotifyWebApi from 'spotify-web-api-node'
+
+interface SpotifyClientConfig {
+  clientId: string
+  clientSecret: string
+}
+
+export class SpotifyClient {
+  session: SpotifyWebApi
+  clientId: string
+  clientSecret: string
+  scope: string[]
+
+  constructor(config: SpotifyClientConfig) {
+    this.clientId = config.clientId
+    this.clientSecret = config.clientSecret
+    this.scope = ['user-library-read',
+      'user-read-currently-playing',
+      'streaming',
+      'user-read-playback-state',
+      'user-read-recently-played']
+    this.session = new SpotifyWebApi({
+      clientId: this.clientId,
+      clientSecret: this.clientSecret
+    })
+  }
+
+  async login() {
+    const url = await this.session.createAuthorizeURL(this.scope, 'state')
+    console.log(url)
+    const data = await this.session.clientCredentialsGrant();
+    this.session.setAccessToken(data.body['access_token'])
+    return data
+
+    // _this.session.clientCredentialsGrant().then(data => {
+    //   // console.log(data.body['access_token'])
+    //   _this.session.setAccessToken(data.body['access_token']);
+    //   console.log('then', _this.session.getAccessToken())
+    // }, error => {
+    //   console.log('Error retrieving an access token', error.message);
+    // });
+
+    // console.log('accesstoken', await _this.session.getAccessToken())
+    // this.session.setAccessToken(this.bearerToken);
+  }
+
+  async fetch() {
+    console.log('fetch', this.session.getAccessToken())
+    this.session.getMyTopArtists()
+      .then(function (data: any) {
+        const topArtists = data.body.items;
+        return topArtists;
+      }, function (err: any) {
+        console.log(JSON.stringify(err));
+      });
+  }
+}
+
+
+
+
+
