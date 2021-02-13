fb15cb8f84fca5d440f9ca8dac669282375b4aa0
Jan Schill <janschill@me.com>
2021-02-13 01:57:56 +0100

    Use client class with async/await

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,35 @@
 import dotenv from "dotenv";
 import express from "express";
-import { Session } from "@inrupt/solid-client-authn-node";
+import { SolidClient } from "./client";
 import { toKebabCase } from "./util/converter"
+import { Session } from "@inrupt/solid-client-authn-node"
 
 dotenv.config()
 
 const app = express();
-const port: string | undefined = process.env.SERVER_PORT;
+const port: string = process.env.SERVER_PORT || '3333'
 const appName: string = process.env.APP_NAME || 'SolidSpotify'
-const clientId: string | undefined = process.env.CLIENT_ID
-const clientSecret: string | undefined = process.env.CLIENT_SECRET
-const refreshToken: string | undefined = process.env.REFRESH_TOKEN
-const oidcIssuer: string | undefined = process.env.OIDC_ISSUER
-const resourceUrl: string = `${oidcIssuer}${toKebabCase(appName)}/tracks.ttl`
+const clientId: string = process.env.CLIENT_ID || 'example-id'
+const clientSecret: string = process.env.CLIENT_SECRET || 'example-secret'
+const refreshToken: string = process.env.REFRESH_TOKEN || 'example-token'
+const oidcIssuer: string = process.env.OIDC_ISSUER || 'https://broker.pod.inrupt.com'
+const privateResourceUrl: string = `https://pod.inrupt.com/jan/${toKebabCase(appName)}/tracks.ttl`
+const publicResourceUrl: string = `https://pod.inrupt.com/jan/public/`
 
-const session: Session = new Session();
-session.login({
-  clientId: clientId,
-  clientSecret: clientSecret,
-  refreshToken: refreshToken,
-  oidcIssuer: oidcIssuer,
-}).then(() => {
-  if (session.info.isLoggedIn) {
-    session
-      .fetch(resourceUrl)
-      .then((response: any) => {
-        return response.text();
-      })
-      .then(console.log);
+async function main() {
+  const solidClient = new SolidClient({
+    clientId: clientId,
+    clientSecret: clientSecret,
+    refreshToken: refreshToken,
+    oidcIssuer: oidcIssuer
+  });
+
+  await solidClient.login();
+
+  if (solidClient.session) {
+    const responsePrivate = await solidClient.fetch(privateResourceUrl);
+    console.log(responsePrivate)
   }
-});
+}
+
+main()
